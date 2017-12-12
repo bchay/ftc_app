c64b6749eb544ee88eb6dee90e2db5ba56f657f9
Bchay <b.chay9@gmail.com>
2017-12-12 16:03:04 -0600

    Final code after second league

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RelicRecoveryAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RelicRecoveryAutonomous.java
@@ -9,6 +9,8 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
 
+import java.nio.channels.ClosedByInterruptException;
+
 @Autonomous(name = "Relic Recovery Autonomous")
 public class RelicRecoveryAutonomous extends OpModeBase { //MecanumTeleop is a LinearOpMode so it can extend the same base class as autonomous
     VuMarkReader vuMarkReader;
@@ -37,9 +39,9 @@ public class RelicRecoveryAutonomous extends OpModeBase { //MecanumTeleop is a L
         //Grab preloaded glyph
         leftGlyphGrabber.setPosition(LEFT_GLYPH_GRABBR_CLOSED);
         rightGlyphGrabber.setPosition(RIGHT_GLYPH_GRABBR_CLOSED);
-        sleep(1000); //Give servos time to move down
+        sleep(800); //Give servos time to move down
         glyphLift.setPower(-1); //Move lift up
-        sleep(400);
+        sleep(500);
         glyphLift.setPower(0);
 
 
@@ -77,23 +79,23 @@ public class RelicRecoveryAutonomous extends OpModeBase { //MecanumTeleop is a L
         }
 
         //Default to close cryptobox column
-        if(vuMark.equals(RelicRecoveryVuMark.UNKNOWN)) vuMark = allianceColor.equals("Red") ? RelicRecoveryVuMark.CENTER : RelicRecoveryVuMark.LEFT;
+        if(vuMark.equals(RelicRecoveryVuMark.UNKNOWN)) vuMark = allianceColor.equals("Blue") ? RelicRecoveryVuMark.LEFT : RelicRecoveryVuMark.RIGHT;
 
         if(location.equals("Side")) {
             move((allianceColor.equals("Blue") ? -1 : 1) * 26);
             turn(90, allianceColor.equals("Blue") ? Direction.RIGHT : Direction.LEFT);
 
-            if(vuMark.equals(RelicRecoveryVuMark.RIGHT)) move(allianceColor.equals("Blue") ? -17 : 25, moveSpeedMax, false);
-            else if(vuMark.equals(RelicRecoveryVuMark.CENTER)) move(allianceColor.equals("Blue") ? -20 : 20, moveSpeedMax, false);
-            else if(vuMark.equals(RelicRecoveryVuMark.LEFT)) move(allianceColor.equals("Blue") ? -25 : 17, moveSpeedMax, false);
+            if(vuMark.equals(RelicRecoveryVuMark.RIGHT)) move(allianceColor.equals("Blue") ? -17 : 3, moveSpeedMax, false);
+            else if(vuMark.equals(RelicRecoveryVuMark.CENTER)) move(allianceColor.equals("Blue") ? -11 : 11, moveSpeedMax, false);
+            else if(vuMark.equals(RelicRecoveryVuMark.LEFT)) move(allianceColor.equals("Blue") ? -3 : 20, moveSpeedMax, false);
 
             turn(90, allianceColor.equals("Blue") ? Direction.RIGHT : Direction.LEFT);
         } else {
-            if(vuMark.equals(RelicRecoveryVuMark.RIGHT)) move(allianceColor.equals("Blue") ? -44 : 29, moveSpeedMax, false);
-            else if(vuMark.equals(RelicRecoveryVuMark.CENTER)) move(allianceColor.equals("Blue") ? -37 : 37, moveSpeedMax, false);
-            else if(vuMark.equals(RelicRecoveryVuMark.LEFT)) move(allianceColor.equals("Blue") ? -29 : 44, moveSpeedMax, false);
+            if(vuMark.equals(RelicRecoveryVuMark.RIGHT)) move(allianceColor.equals("Blue") ? -46 : 30, moveSpeedMax, false);
+            else if(vuMark.equals(RelicRecoveryVuMark.CENTER)) move(allianceColor.equals("Blue") ? -37.5 : 37.5, moveSpeedMax, false);
+            else if(vuMark.equals(RelicRecoveryVuMark.LEFT)) move(allianceColor.equals("Blue") ? -30 : 46, moveSpeedMax, false);
 
-            turn(90, Direction.RIGHT, turnSpeed, 0, 5000);
+            turn(90, Direction.RIGHT, turnSpeed - .1, 0, 5000);
         }
 
         move(8, moveSpeedMax, false, 0, 2000); //Move forward into cryptobox
@@ -109,6 +111,11 @@ public class RelicRecoveryAutonomous extends OpModeBase { //MecanumTeleop is a L
         sleep(1200);
 
         move(-5, moveSpeedMax);
+
+        leftGlyphGrabber.setPosition(LEFT_GLYPH_GRABBR_CLOSED); //Move servos into closed position before driving into glyph
+        rightGlyphGrabber.setPosition(RIGHT_GLYPH_GRABBR_CLOSED);
+        sleep(1000);
+
         move(7, moveSpeedMax, false, 0, 2000); //Timeout because robot may hit crptobox
         move(-8, moveSpeedMax);
     }
